79c637b42ca35a2ecd124bc9c9ca7cc17dcc0c60
Johannes Hildén <hildenjohannes@gmail.com>
2018-11-14 07:59:35 +0300

    stub out api

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
 import express from 'express'; 
 
-const app = express();
+const app  = express();
+const port = process.env.PORT || 4399;
 
-app.get('/', (req, res) => {
-  res.send('Hello World');
+app.get('/start', (req, res) => {
+  res.json({
+    game: {
+      id: 1
+    }
+  });
 });
 
-app.listen(4399, () => {
-  console.log('Listening');
+app.get('/roll/:game_id', (req, res) => {
+  const id = req.params['game_id'];
+  res.json({
+    game: {
+      id: id
+    }
+  });
+});
+
+app.get('/stats/:game_id', (req, res) => {
+  res.json({
+    stats: { error: 'not implemented' }
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
 });
