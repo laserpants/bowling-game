ef8e72b4752df6e954bff9979a5170a69c78e345
Johannes Hildén <hildenjohannes@gmail.com>
2018-11-17 12:34:21 +0300

    build out component

--- client/src/index.js
+++ client/src/index.js
@@ -1,9 +1,42 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const title = 'My Minimal React Webpack Babel Setup';
+class Game extends React.Component {
+
+  constructor() {
+    super();
+  }
+
+  startGame() {
+    console.log('start game');
+    fetch('http://localhost:4399/games', {
+      method: 'POST',
+      mode: 'cors',
+      credentials: 'same-origin',
+      headers: {
+        'Content-Type': 'application/json; charset=utf-8',
+      }
+    })
+    .then(response => response.json())
+    .then(json => {
+      console.log(json);
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        Hello
+        <button onClick={this.startGame}>
+          Start a game
+        </button>
+      </div>
+    );
+  }
+
+}
 
 ReactDOM.render(
-  <div>{title}</div>,
+  <Game />,
   document.getElementById('app')
 );
