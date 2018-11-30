79485418f72224261efae1cef9a47a9ddecac315
Johannes Hildén <hildenjohannes@gmail.com>
2018-11-30 12:00:02 +0300

    fix webpack setup

--- elm-client/webpack.config.js
+++ elm-client/webpack.config.js
@@ -1,4 +1,5 @@
 var path = require('path');
+var CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
 
@@ -9,7 +10,8 @@ module.exports = {
         test: /\.html$/,
         exclude: /node_modules/,
         loader: 'file-loader?name=[name].[ext]'
-      }, {
+      }, 
+      {
         test: /\.elm$/,
         exclude: [/elm-stuff/, /node_modules/],
         loader: 'elm-webpack-loader',
@@ -19,6 +21,13 @@ module.exports = {
       }
     ]
   },
+
+  plugins: [
+    new CopyWebpackPlugin([  
+      { from: 'src/css/*', to: 'dist/css/' },
+      { from: 'src/img/*', to: 'dist/img/' }
+    ], {})
+  ],
 
   devServer: {
     inline: true,
